f17e7375ff1c2e551d11815e22b718d47469bf7a
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-06 10:48:12 -0500

    restructure index via App.js, and all associated styles, content, and components.

--- src/App.js
+++ src/App.js
@@ -12,16 +12,17 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
-          <div class="w3-display-middle" style={{whiteSpace:'nowrap'}}></div></div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Pete, Data, Chepe and Pancho Working!</h1>
+          <header className="App-header">
         </header>
+        <br></br><br></br><br></br><br></br><div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+          <div class="w3-display-middle" style={{whiteSpace:'nowrap'}}></div>
+          <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Code <span class="w3-hide-small">Monkey</span> Dojo</span></div>
+          <br></br><img src={logo} className="App-logo" alt="logo" /><br></br>
+          <h1 className="App-title">Pete, Data, Chepe and Pancho Working!</h1>
         <p className="App-intro">
           All rights reserved
         </p>
-        {/*}<CenterDiv />
+        {/*<CenterDiv />
         <VideoComponent />*/}
         <Footer />
       </div>
