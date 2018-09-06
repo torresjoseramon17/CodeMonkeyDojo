857dd848757952e6c727423f6e07c2ad040e8d8d
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-06 10:32:55 -0500

    init, fix missing css React imports, comment out unused components, restructure template style attributes for React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './resources/logo.gif';
 import './App.css';
+import './index.css';
 import Navbar from './components/navbar.js'
 import CenterDiv from './components/centerDiv.js'
 import Footer from './components/footer.js'
@@ -11,15 +12,17 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+          <div class="w3-display-middle" style={{whiteSpace:'nowrap'}}></div></div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Pete,Data,Chepe and Pancho Working!</h1>
+          <h1 className="App-title">Pete, Data, Chepe and Pancho Working!</h1>
         </header>
         <p className="App-intro">
-          All rights reserved <code>src/App.js</code>
+          All rights reserved
         </p>
-        <CenterDiv />
-        <VideoComponent />
+        {/*}<CenterDiv />
+        <VideoComponent />*/}
         <Footer />
       </div>
     );
