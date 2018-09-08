b7b85b3ad4f0dfccc036a803dc578834ff7cdf4c
Xol3 <xoalxiyetl@gmail.com>
2018-09-08 14:46:21 -0500

    added files for DB and structure

---
+++ database/data.js
@@ -0,0 +1,6 @@
+//Initialize repo
+module.exports = {
+  Users: [
+          {type:1, user_name: "Test", email: "test@test.com", password: "TEST"}
+          ]
+};
---
+++ database/index.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Asus1!',
+  database: 'MonkeyDB'
+});
+
+// connection.connect();
+// var Users = {
+//   type : 1,
+//   user_name: "Pancho",
+//   email: "PanchoLOCO@Poblanos.com",
+//   password: "Macaque1!"
+// };
+
+const selectAll = function(cb) {
+  connection.query("SELECT * FROM Users", (err, results, fields) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, results);
+    }
+  });
+};
+
+const insertOne = function(type, user_name,email,password, cb) {
+  connection.query(
+    "INSERT INTO Users (type, user_name,email,password) VALUES (?, ?)",
+    [type, user_name, email, password],
+    (err, results, fields) => {
+      if (err) {
+        cb(err, null);
+      } else {
+        console.log(results);
+        cb(null, results);
+      }
+    }
+  );
+};
+
+module.exports.selectAll = selectAll;
+module.exports.insertOne = insertOne;
